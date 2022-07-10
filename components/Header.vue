7638bd11274c4ef8b1b2513e40dd4682aea524da
Agus Andiyono <andiyono.id@gmail.com>
2022-07-10 12:20:51 +0700

    add footer

--- components/Header.vue
+++ components/Header.vue
@@ -2,7 +2,7 @@
   <div>
   <!-- ====== Navbar Section Start -->
     <div
-      class="ud-header absolute top-0 left-0 z-40 flex w-full items-center bg-white"
+      class="ud-header absolute top-0 left-0 z-40 flex w-full items-center bg-white shadow-lg"
     >
       <div class="container">
         <div class="relative -mx-4 flex items-center justify-between">
@@ -35,15 +35,15 @@
                   <li class="group relative">
                     <a
                       href="#home"
-                      class="ud-menu-scroll mx-8 flex py-2 text-base text-dark group-hover:text-primary lg:mr-0 lg:inline-flex lg:py-6 lg:px-0 lg:text-gray lg:group-hover:text-gray lg:group-hover:opacity-70"
+                      class="ud-menu-scroll mx-8 flex py-2 text-base text-dark group-hover:text-green-600 lg:mr-0 lg:inline-flex lg:py-6 lg:px-0 lg:text-gray lg:group-hover:text-gray lg:group-hover:opacity-70"
                     >
                       Beranda
                     </a>
                   </li>
                   <li class="group relative">
                     <a
-                      href="/#about"
-                      class="ud-menu-scroll mx-8 flex py-2 text-base text-dark group-hover:text-primary lg:mr-0 lg:ml-7 lg:inline-flex lg:py-6 lg:px-0 lg:text-gray lg:group-hover:text-gray lg:group-hover:opacity-70 xl:ml-12"
+                      href="/#sponsor"
+                      class="ud-menu-scroll mx-8 flex py-2 text-base text-dark group-hover:text-green-600 lg:mr-0 lg:ml-7 lg:inline-flex lg:py-6 lg:px-0 lg:text-gray lg:group-hover:text-gray lg:group-hover:opacity-70 xl:ml-12"
                     >
                       Support
                     </a>
@@ -51,7 +51,7 @@
                   <li class="group relative">
                     <a
                       href="/#lomba"
-                      class="ud-menu-scroll mx-8 flex py-2 text-base text-dark group-hover:text-primary lg:mr-0 lg:ml-7 lg:inline-flex lg:py-6 lg:px-0 lg:text-gray lg:group-hover:text-gray lg:group-hover:opacity-70 xl:ml-12"
+                      class="ud-menu-scroll mx-8 flex py-2 text-base text-dark group-hover:text-green-600 lg:mr-0 lg:ml-7 lg:inline-flex lg:py-6 lg:px-0 lg:text-gray lg:group-hover:text-gray lg:group-hover:opacity-70 xl:ml-12"
                     >
                       Lomba
                     </a>
@@ -59,7 +59,7 @@
                   <li class="group relative">
                     <a
                       href="/#team"
-                      class="ud-menu-scroll mx-8 flex py-2 text-base text-dark group-hover:text-primary lg:mr-0 lg:ml-7 lg:inline-flex lg:py-6 lg:px-0 lg:text-gray lg:group-hover:text-gray lg:group-hover:opacity-70 xl:ml-12"
+                      class="ud-menu-scroll mx-8 flex py-2 text-base text-dark group-hover:text-green-600 lg:mr-0 lg:ml-7 lg:inline-flex lg:py-6 lg:px-0 lg:text-gray lg:group-hover:text-gray lg:group-hover:opacity-70 xl:ml-12"
                     >
                       Daftar
                     </a>
@@ -67,7 +67,7 @@
                   <li class="group relative">
                     <a
                       href="/#contact"
-                      class="ud-menu-scroll mx-8 flex py-2 text-base text-dark group-hover:text-primary lg:mr-0 lg:ml-7 lg:inline-flex lg:py-6 lg:px-0 lg:text-gray lg:group-hover:text-gray lg:group-hover:opacity-70 xl:ml-12"
+                      class="ud-menu-scroll mx-8 flex py-2 text-base text-dark group-hover:text-green-600 lg:mr-0 lg:ml-7 lg:inline-flex lg:py-6 lg:px-0 lg:text-gray lg:group-hover:text-gray lg:group-hover:opacity-70 xl:ml-12"
                     >
                       Kontak
                     </a>
@@ -81,21 +81,25 @@
     </div>
     <!-- ====== Navbar Section End -->
 
+
     <!-- ====== Hero Section Start -->
     <div
       id="home"
       class="relative overflow-hidden bg-transparent pt-[120px] md:pt-[130px] lg:pt-[160px]"
     >
-      <div class="container">
+      <div class="container px-4">
         <div class="-mx-4 flex flex-wrap items-center">
           <img class="object-cover" src="banner.png">
         </div>
+        <p class="text-center text-gray-600">
+          IFOS ( Islamic Festival Of Smanexix ) adalah sebuah festival seni dan budaya Islam serta kompetisi pengetahuan Islam yang diadakan oleh Sie Kerohanian Islam  (SKI) SMAN 19 Surabaya, dan merupakan program kerja terbesar di setiap tahunnya.
+        </p>
       </div>
     </div>
     <!-- ====== Hero Section End -->
 
     <!-- ====== Hero Sponsor Start -->
-    <section id="testimonials" class="pt-20 md:pt-[120px]">
+    <section id="sponson" class="pt-5 md:pt-[120px]">
       <div class="container px-4">
         <div class="flex flex-wrap">
           <div class="mx-4 w-full">
@@ -306,104 +310,49 @@
     </section>
     <!-- ====== Testimonials End ====== -->
 
-    <!-- ====== Hero Sponsor Start -->
-    <section id="testimonials" class="pt-20">
+
+<!-- ====== Lomba Start ====== -->
+    <section id="lomba" class="px-5 md:pt-[120px]">
       <div class="container px-4">
         <div class="flex flex-wrap">
           <div class="mx-4 w-full">
             <div class="mx-auto mb-[60px] max-w-[620px] text-center lg:mb-20">
               <h2
-                class="mb-4 text-3xl font-bold sm:text-4xl md:text-[42px]"
+                class="mb-4 text-3xl font-bold text-dark sm:text-4xl md:text-[42px]"
               >
                 TIMELINE
               </h2>
             </div>
           </div>
         </div>
-        <!-- Container -->
-        <div class="max-w-screen-xl mx-auto px-4">
-          <!-- Grid wrapper -->
-          <div class="-mx-4 flex flex-wrap">
-            <!-- Grid column -->
-            <div class="w-full flex flex-col p-4 sm:w-1/2 lg:w-1/4">
-              <!-- Column contents -->
-              <div
-                class="flex flex-col flex-1 px-10 py-12 bg-white rounded-lg shadow-lg text-center"
-              >
-                <div class="flex-1">
-                  <h2 class="text-gray-900 text-2xl font-bold leading-snug">
-                    Panduan Lomba
-                  </h2>
-                  <p class="mt-2 text-lg">
-                    Download disini
-                  </p>
-                </div>
-                <a
-                  href="#"
-                  class="mt-6 inline-flex items-center px-6 py-3 text-white font-semibold bg-green-700 rounded-md shadow-sm text-center"
-                >
-                  Download
-                </a>
+        <div class="flex flex-wrap">
+          <div class="w-full px-4 pb-4 md:w-1/2 lg:w-1/4">
+              <!-- <img class="header-logo" src="timeline/timeline1.png"> -->
+              <!-- <div class="relative">
+                <img class="w-full" src="timeline/timeline1.png" alt="dummy-image">
+                <button class="absolute bottom-0 bg-green-500 rounded-full text-white p-2 hover:bg-green-800 m-2">Button</button>
+              </div> -->
+              <div class="relative">
+                <img src="timeline/timeline1.png" />
+                <button class="absolute bg-green-500 hover:bg-green-800 rounded-full text-white px-10 py-2 bottom-4 left-1/2 -translate-x-1/2">Download</button>
               </div>
-            </div>
-            <!-- Grid column -->
-            <div class="w-full flex flex-col p-4 sm:w-1/2 lg:w-1/4">
-              <!-- Column contents -->
-              <div
-                class="flex flex-col flex-1 px-10 py-12 bg-white rounded-lg shadow-lg"
-              >
-                <div class="flex-1">
-                  <h2 class="text-gray-900 text-2xl font-bold leading-snug">
-                    Pendaftaran
-                  </h2>
-                  <p class="mt-2 text-lg">
-                    20 Juli <br/>
-                    Offline & Online
-                  </p>
-                </div>
-              </div>
-            </div>
-            <!-- Grid column -->
-            <div class="w-full flex flex-col p-4 sm:w-1/2 lg:w-1/4">
-              <!-- Column contents -->
-              <div
-                class="flex flex-col flex-1 px-10 py-12 bg-white rounded-lg shadow-lg text-center"
-              >
-                <div class="flex-1">
-                  <h2 class="text-gray-900 text-2xl font-bold leading-snug">
-                    Technical Meeting
-                  </h2>
-                  <p class="mt-2 text-lg">
-                    10 September
-                  </p>
-                </div>
-              </div>
-            </div>
-            <!-- Grid column -->
-            <div class="w-full flex flex-col p-4 sm:w-1/2 lg:w-1/4">
-              <!-- Column contents -->
-              <div
-                class="flex flex-col flex-1 px-10 py-12 bg-white rounded-lg shadow-lg text-center"
-              >
-                <div class="flex-1">
-                  <h2 class="text-gray-900 text-2xl font-bold leading-snug">
-                    Perlombaan
-                  </h2>
-                  <p class="mt-2 text-lg">
-                    24 Oktober
-                  </p>
-                </div>
-
-              </div>
-            </div>
+          </div>
+          <div class="w-full px-4 pb-4 md:w-1/2 lg:w-1/4">
+              <img class="header-logo" src="timeline/timeline2.png">
+          </div>
+          <div class="w-full px-4 pb-4 md:w-1/2 lg:w-1/4">
+              <img class="header-logo" src="timeline/timeline3.png">
+          </div>
+          <div class="w-full px-4 pb-4 md:w-1/2 lg:w-1/4">
+              <img class="header-logo" src="timeline/timeline4.png">
           </div>
         </div>
       </div>
     </section>
-    <!-- ====== Hero Section End -->
+    <!-- ====== Testimonials End ====== -->
 
     <!-- ====== Lomba Start ====== -->
-    <section id="lomba" class="pt-10 pb-10 md:pt-[120px] bg-gray-200">
+    <section id="lomba" class="px-5 md:pt-[120px] bg-gray-200">
       <div class="container px-4">
         <div class="flex flex-wrap">
           <div class="mx-4 w-full">
@@ -421,7 +370,7 @@
             <p class="text-gray-400"> Online: </p>
             <a
               href="#"
-              class="mt-6 items-center px-6 py-3 text-lg w-full block text-white font-semibold bg-green-700 rounded-md shadow-sm text-center"
+              class="mt-6 items-center px-6 py-3 text-lg w-full block text-white font-semibold bg-green-500 hover:bg-green-800 rounded-full shadow-sm text-center"
             >
               Daftar Online
             </a>
@@ -437,5 +386,68 @@
       </div>
     </section>
     <!-- ====== Testimonials End ====== -->
+
+    <!-- ====== Lomba Start ====== -->
+    <section id="lomba" class="px-5 md:pt-[120px]">
+      <div class="container px-4">
+        <div class="flex flex-wrap">
+          <div class="mx-4 w-full">
+            <div class="mx-auto mb-[60px] max-w-[620px] text-center lg:mb-20">
+              <h2
+                class="mb-4 text-3xl font-bold text-dark sm:text-4xl md:text-[42px]"
+              >
+                KONTAK KAMI
+              </h2>
+            </div>
+          </div>
+        </div>
+        <div class="flex flex-wrap">
+          <div class="w-full px-4 pb-4 md:w-1/2 lg:w-1/5">
+            <div class="peta" style="width:800px">
+                <div class="embed-responsive embed-responsive-21by9">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d924.8566217993155!2d112.76408042162582!3d-7.0436112637492005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd805ed4a3e99fb%3A0x4f5847d3d4e262f0!2sSolite%20Kids!5e0!3m2!1sid!2sid!4v1647918104012!5m2!1sid!2sid" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </div>
+            </div>
+          </div>
+          <div class="w-full px-4 pb-4 md:w-1/2 lg:w-1/5">
+            <div class="flex">
+              <div class="w-full px-4 pb-4 md:w-1/2 lg:w-1/5">
+                <img src="sosmed/whatsapp.png">
+              </div>
+              <div class="w-full px-4 pb-4 md:w-1/2 lg:w-1/5">
+                <p class="text-green-600">0858 1099 8001</p>
+                <p class="text-green-600">0896 1615 3872</p>
+              </div>
+            </div>
+            <div class="flex">
+              <div class="w-full px-4 pb-4 md:w-1/2 lg:w-1/5">
+                <img src="sosmed/web.png">
+              </div>
+              <div class="w-full px-4 pb-4 md:w-1/2 lg:w-1/5">
+                <p class="text-green-600">www.ifos5skinexix.com</p>
+              </div>
+            </div>
+            <div class="flex">
+              <div class="w-full px-4 pb-4 md:w-1/2 lg:w-1/5">
+                <img src="sosmed/ig.png">
+              </div>
+              <div class="w-full px-4 pb-4 md:w-1/2 lg:w-1/5">
+                <p class="text-green-600">ifos.skinexixsby</p>
+              </div>
+            </div>
+            <div class="flex">
+              <div class="w-full px-4 pb-4 md:w-1/2 lg:w-1/5">
+                <img src="sosmed/gmail.png">
+              </div>
+              <div class="w-full px-4 pb-4 md:w-1/2 lg:w-1/5">
+                <p class="text-green-600">ifos.skinexixsby@gmail.com</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- ====== Testimonials End ====== -->
+
   </div>
 </template>
---
+++ components/Timeline.vue
@@ -0,0 +1,97 @@
+<template>
+   <!-- ====== Timeline Start -->
+    <section id="testimonials" class="pt-20">
+      <div class="container px-4">
+        <div class="flex flex-wrap">
+          <div class="mx-4 w-full">
+            <div class="mx-auto mb-[60px] max-w-[620px] text-center lg:mb-20">
+              <h2
+                class="mb-4 text-3xl font-bold sm:text-4xl md:text-[42px]"
+              >
+                TIMELINE
+              </h2>
+            </div>
+          </div>
+        </div>
+        <!-- Container -->
+        <div class="max-w-screen-xl mx-auto px-4">
+          <!-- Grid wrapper -->
+          <div class="-mx-4 flex flex-wrap">
+            <!-- Grid column -->
+            <div class="w-full flex flex-col p-4 sm:w-1/2 lg:w-1/4">
+              <!-- Column contents -->
+              <div
+                class="flex flex-col flex-1 px-10 py-12 bg-white rounded-lg shadow-lg text-center"
+              >
+                <div class="flex-1">
+                  <h2 class="text-gray-900 text-2xl font-bold leading-snug">
+                    Panduan Lomba
+                  </h2>
+                  <p class="mt-2 text-lg">
+                    Download disini
+                  </p>
+                </div>
+                <a
+                  href="#"
+                  class="mt-6 inline-flex items-center px-6 py-3 text-white font-semibold bg-green-700 rounded-md shadow-sm text-center"
+                >
+                  Download
+                </a>
+              </div>
+            </div>
+            <!-- Grid column -->
+            <div class="w-full flex flex-col p-4 sm:w-1/2 lg:w-1/4">
+              <!-- Column contents -->
+              <div
+                class="flex flex-col flex-1 px-10 py-12 bg-white rounded-lg shadow-lg"
+              >
+                <div class="flex-1">
+                  <h2 class="text-gray-900 text-2xl font-bold leading-snug">
+                    Pendaftaran
+                  </h2>
+                  <p class="mt-2 text-lg">
+                    20 Juli <br/>
+                    Offline & Online
+                  </p>
+                </div>
+              </div>
+            </div>
+            <!-- Grid column -->
+            <div class="w-full flex flex-col p-4 sm:w-1/2 lg:w-1/4">
+              <!-- Column contents -->
+              <div
+                class="flex flex-col flex-1 px-10 py-12 bg-white rounded-lg shadow-lg text-center"
+              >
+                <div class="flex-1">
+                  <h2 class="text-gray-900 text-2xl font-bold leading-snug">
+                    Technical Meeting
+                  </h2>
+                  <p class="mt-2 text-lg">
+                    10 September
+                  </p>
+                </div>
+              </div>
+            </div>
+            <!-- Grid column -->
+            <div class="w-full flex flex-col p-4 sm:w-1/2 lg:w-1/4">
+              <!-- Column contents -->
+              <div
+                class="flex flex-col flex-1 px-10 py-12 bg-white rounded-lg shadow-lg text-center"
+              >
+                <div class="flex-1">
+                  <h2 class="text-gray-900 text-2xl font-bold leading-snug">
+                    Perlombaan
+                  </h2>
+                  <p class="mt-2 text-lg">
+                    24 Oktober
+                  </p>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- ====== Hero Section End -->
+</template>
